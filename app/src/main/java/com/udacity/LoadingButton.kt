e0a758bad79fb8fe1af6ffb13cb48df9db88630a
cstimler <charles.ces@gmail.com>
2021-08-29 04:17:29 -0700

    Substantial progress on button animations and button custom view (basically completed).  Have linked button state to main activity.  Still need to work on notifications and detail screen.

--- app/src/main/java/com/udacity/LoadingButton.kt
+++ app/src/main/java/com/udacity/LoadingButton.kt
@@ -3,38 +3,55 @@ package com.udacity
 import android.animation.Animator
 import android.animation.ValueAnimator
 import android.content.Context
+import android.content.res.Resources
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.Rect
 import android.renderscript.Sampler
 import android.util.AttributeSet
 import android.util.Log
+import android.util.TypedValue
 import android.view.View
 import androidx.core.content.withStyledAttributes
 import kotlinx.android.synthetic.main.content_main.view.*
 import java.lang.Math.PI
+import kotlin.math.roundToInt
 import kotlin.properties.Delegates
 
 class LoadingButton @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
-    private var widthSize = 100
-    private var heightSize = 100
+    private var widthSize = 0f
+    private var heightSize = 0f
     private var backgroundColoring = 0
     private var mainTextColor = 0
+    private var darkOverlayColor = 0
+    private var circleColor = 0
+    private var buttonText: String = "Download"
+    private lateinit var globalCanvas: Canvas
 
- //   private val valueAnimator = ValueAnimator()
-    private var sweepAngle: Float = 90f
+    //   private val valueAnimator = ValueAnimator()
+    private var sweepAngle: Float = 0f
+    private var rightSideOfMovingRectangle: Float = 20f
 
-    private var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+    private var stopAnimatingNow: Boolean = false
+
+    var buttonState: ButtonState by Delegates.observable<ButtonState>(ButtonState.Completed) { p, old, new ->
+        // https://knowledge.udacity.com/questions/420421
+        when(new) {
+            ButtonState.Loading -> animateView()
+            ButtonState.Completed -> stopAnimation()
+            else -> Log.i("CHARLES when statement problem", "got to else")
+        }
+
     }
-    var v2 = getRootView()
-    var rv = v2.custom_button
     private var paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
         style = Paint.Style.FILL
         textAlign = Paint.Align.CENTER
-        textSize = 50.0f
-       // color = Color.BLACK
+        val desiredSPSize = 20f
+        textSize = scaleForSP(desiredSPSize)
+        // color = Color.BLACK
         color = backgroundColoring
     }
 
@@ -42,24 +59,46 @@ class LoadingButton @JvmOverloads constructor(
         context.withStyledAttributes(attrs, R.styleable.LoadingButton) {
             backgroundColoring = getColor(R.styleable.LoadingButton_backgroundColoring, 0)
             mainTextColor = getColor(R.styleable.LoadingButton_mainTextColor, 0)
-
+            darkOverlayColor = getColor(R.styleable.LoadingButton_darkOverlayColor, 0)
+            circleColor = getColor(R.styleable.LoadingButton_circleColor, 0)
         }
+    }
+
+    fun stopAnimation() {
+        stopAnimatingNow = true
     }
 
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        // Udacity knowledge: https://knowledge.udacity.com/questions/672428
+        // helped with next few lines
+        var z = Rect()
+        canvas?.getClipBounds(z)
+        heightSize = z.height().toFloat()
+        widthSize = z.width().toFloat()
         paint.color = backgroundColoring
-        canvas?.drawRect(30f, 30f, 1200f, 800f, paint)
-      //  paint.color = Color.RED
+        canvas?.drawRect(20f, 20f, widthSize, heightSize, paint)
+        paint.color = darkOverlayColor
+        canvas?.drawRect(20f, 20f, rightSideOfMovingRectangle, heightSize, paint)
+        //  paint.color = Color.RED
         paint.color = mainTextColor
-        canvas?.drawText("TESTING", 500f, 130f, paint)
-        canvas?.drawArc(650f, 60f, 750f, 160f, 0f, sweepAngle, true, paint)
-        Log.i("CHARLES in onDraw:", sweepAngle.toString())
-        Log.i("CHARLES IN onDraw - update regular sweepAngle", sweepAngle.toString())
-        Log.i("CHARLES GENERIC ONDRAW","THIS JUST HAS TO BE CALLED")
+        // paint.getTextBounds(buttonText, 0, buttonText.length, z)
+        canvas?.drawText(buttonText, widthSize / 2, heightSize / 2 + 10, paint)
+       // Log.i(
+      //      "CHARLES: left, bottom",
+      //      z.left.toFloat().toString() + " " + z.bottom.toFloat().toString()
+     //   )
+        paint.color = circleColor
+        canvas?.drawArc(widthSize-heightSize, heightSize/4, widthSize-heightSize/2, 3*heightSize/4, 0f, sweepAngle, true, paint)
+     //   Log.i("CHARLES in onDraw:", sweepAngle.toString())
+     //   Log.i("CHARLES IN onDraw - update regular sweepAngle", sweepAngle.toString())
+     //   Log.i("CHARLES GENERIC ONDRAW", "THIS JUST HAS TO BE CALLED")
     }
 
+
+
+    /*
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
         val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
@@ -72,28 +111,42 @@ class LoadingButton @JvmOverloads constructor(
         heightSize = h
         setMeasuredDimension(w, h)
     }
+ */
 // stackoverflow.com/questions/4946295/android-expand-collapse-animation
-    companion object fun animateView(v:View, initialAngle: Float, finalAngle: Float) {
+    companion object fun animateView() {
         Log.i("CHARLES", "Got into animate View")
         val valueAnimator = ValueAnimator.ofFloat(0f, 360.0f)
         valueAnimator.addUpdateListener { animation ->
             sweepAngle = animation.animatedValue as Float
-           // requestLayout()
+            rightSideOfMovingRectangle =
+                (animation.animatedValue as Float) * ((widthSize - 30f) / 360) + 30f
+       //     Log.i("CHARLES - right side of moving rect is:", rightSideOfMovingRectangle.toString())
+            // requestLayout()
             invalidate()
-            setBackgroundColor(Color.BLUE)
-            Log.i("CHARLES", rv.sweepAngle.toString())
-            Log.i("CHARLES animateView's sweepAngle", sweepAngle.toString())
+            //   setBackgroundColor(Color.BLUE)
+      //      Log.i("CHARLES animateView's sweepAngle", sweepAngle.toString())
         }
-        valueAnimator.addListener(object: Animator.AnimatorListener {
+        valueAnimator.addListener(object : Animator.AnimatorListener {
             override fun onAnimationStart(p0: Animator?) {
-
+                buttonText = "We are loading"
+                custom_button.isEnabled = false
+                stopAnimatingNow = false
+                //lets get the coordinates for the button:
+                //Based on: https://knowledge.udacity.com/questions/672428
             }
 
             override fun onAnimationEnd(p0: Animator?) {
-                sweepAngle = 360f
-                Log.i("CHARLES", "Gets to onAnimationEnd")
-                setBackgroundColor(Color.YELLOW)
-
+                if (stopAnimatingNow) {
+                    sweepAngle = 0f
+                    buttonText = "Download"
+                    rightSideOfMovingRectangle = 20f
+                    custom_button.isEnabled = true
+                    Log.i("CHARLES", "Gets to onAnimationEnd")
+                    //     setBackgroundColor(Color.YELLOW)
+                    //  animateView()
+                } else {
+                    animateView()
+                }
             }
 
             override fun onAnimationCancel(p0: Animator?) {
@@ -105,9 +158,16 @@ class LoadingButton @JvmOverloads constructor(
             }
 
         })
-    valueAnimator.duration = 1000
-    Log.i("CHARLES", "Gets down to start")
-    valueAnimator.start()
+        valueAnimator.duration = 1000
+        Log.i("CHARLES", "Gets down to start")
+        valueAnimator.start()
+    }
+
+
+    // https://stackoverflow.com/questions/3061930/how-to-set-unit-for-paint-settextsize
+    fun scaleForSP(spValue: Float) : Float {
+     val pixels =  TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, Resources.getSystem().displayMetrics)
+        return pixels
     }
 
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -10,7 +10,11 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,27 +29,42 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var radioGroup: RadioGroup
+   // private lateinit var radioButton: RadioButton
+  //  private var broadcastExtraId: Long? = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        radioGroup = findViewById<RadioGroup>(R.id.radio_group)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-         //   download()
-             // scaleButton()
-            testFunc2(it)
+           // download()
+            val response = findOutWhichRadioButtonIsSelected()
+            if (response == "NONE") {
+              Toast.makeText(applicationContext, "Please select file to download", Toast.LENGTH_SHORT).show()
+            }
+            else
+            {download(response)
+                custom_button.buttonState = ButtonState.Loading}
+            // This was helpful: https://knowledge.udacity.com/questions/420421
+           // clickMade()
         }
     }
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             val id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
+            if ( id == downloadID) {
+                custom_button.buttonState = ButtonState.Completed
+            }
         }
     }
 
-    private fun download() {
+    private fun download(URL: String) {
         val request =
             DownloadManager.Request(Uri.parse(URL))
                 .setTitle(getString(R.string.app_name))
@@ -60,20 +79,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
-            "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val glideURL =
+            "https://github.com/bumptech/glide/releases/download/v3.6.0/glide-3.6.0.jar"
+       // private const val udacityURL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter"
+        private const val udacityURL = "https://www.woopsywoopsy.com"
+        private const val retrofitURL = "https://github.com/square/retrofit"
         private const val CHANNEL_ID = "channelId"
     }
-
+/*
     private fun scaleButton() {
         val animator = ObjectAnimator.ofFloat(custom_button, View.SCALE_X, 0.1f)
         animator.duration = 1000
         animator.start()
     }
+*/
 
+    fun clickMade() {
+       custom_button.animateView()
+    }
 
+    fun findOutWhichRadioButtonIsSelected() : String {
+        val radioSelected = radioGroup.checkedRadioButtonId
+        Log.i("CHARLESradio selected:", R.id.radio_udacity.toString())
+        return when (radioSelected) {
+            R.id.radio_glide -> glideURL
+            R.id.radio_udacity -> udacityURL
+            R.id.radio_retrofit -> retrofitURL
+            else -> "NONE"
+        }
 
-    fun testFunc2(view: View) {
-       custom_button.animateView(view, 0f, 360.0f)
     }
 }
