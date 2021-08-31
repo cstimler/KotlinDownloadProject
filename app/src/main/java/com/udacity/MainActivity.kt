cfc4ed586f39cb14a51344c3339928512c4e2a20
cstimler <charles.ces@gmail.com>
2021-08-31 04:26:46 -0700

    Notification boilerplate in place, basic notification working.  Next need to tie into detail view w/data payload.

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.udacity
 
 import android.animation.ObjectAnimator
-import android.app.DownloadManager
-import android.app.NotificationManager
-import android.app.PendingIntent
+import android.app.*
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
@@ -11,9 +9,7 @@ import android.content.IntentFilter
 import android.net.ConnectivityManager
 import android.net.NetworkInfo
 import android.net.Uri
-import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
+import android.os.*
 import android.util.Log
 import android.view.View
 import android.widget.RadioButton
@@ -21,11 +17,15 @@ import android.widget.RadioGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
+import com.udacity.Utils.NotificationUtils.*
 
+private const val CHANNEL_ID = "download_channel"
+private const val CHANNEL_NAME = "download_result"
 
-class MainActivity : AppCompatActivity() {
+class MainActivity  : AppCompatActivity() {
 
     private var downloadID: Long = 0
 
@@ -34,14 +34,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var action: NotificationCompat.Action
 
     private lateinit var radioGroup: RadioGroup
+
     // id for EXTRA_DOWNLOAD_ID
     private var id: Long? = -1
-   // private lateinit var radioButton: RadioButton
-  //  private var broadcastExtraId: Long? = -1
+    // private lateinit var radioButton: RadioButton
+    //  private var broadcastExtraId: Long? = -1
 
     private lateinit var downloadManager: DownloadManager
     // flag to shut down looper after the download completed
-    private var flag = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,8 +52,11 @@ class MainActivity : AppCompatActivity() {
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
+        // get notification channel created early
+        createChannel(CHANNEL_ID, CHANNEL_NAME)
+
         custom_button.setOnClickListener {
-           // download()
+            // download()
             // check for internet connectivity:
             // https://developer.android.com/training/monitoring-device-state/connectivity-status-type
 
@@ -64,16 +67,23 @@ class MainActivity : AppCompatActivity() {
             // if no connection provide warning and return:
 
             if (!isConnected) {
-                Toast.makeText(applicationContext, "No internet connectivity, try again later.", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    "No internet connectivity, try again later.",
+                    Toast.LENGTH_SHORT
+                ).show()
                 return@setOnClickListener
             }
 
             val response = findOutWhichRadioButtonIsSelected()
             if (response == "NONE") {
-              Toast.makeText(applicationContext, "Please select file to download", Toast.LENGTH_SHORT).show()
-            }
-            else
-            {download(response)
+                Toast.makeText(
+                    applicationContext,
+                    "Please select file to download",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                download(response)
                 custom_button.buttonState = ButtonState.Loading
                 Log.i("CHARLESS", "at top of else in click listener")
                 // https://stackoverflow.com/questions/65488410/handler-postdelayed-is-now-deprecated-what-function-to-call-instead
@@ -83,33 +93,44 @@ class MainActivity : AppCompatActivity() {
                 // previously entered but not completed (say the last download happened quickly and this
                 // loop is still running when we request the next download).  In that case the flag will still be "true" and the next loop to execute
                 // will have flag = false and will not print out, leaving the current loop to complete.
-                flag = !flag
-                    Handler(Looper.getMainLooper()).postDelayed({
-                        if (!(downloadID == id) && (flag == true)) {
-                            // signal to the custom button that download still not completed
-                            custom_button.buttonState = ButtonState.Indeterminate
-                            //https://stackoverflow.com/questions/39271027/cancel-download-in-downloadmanager
-                            // stop the download, its taking too long for such a small file
-                            downloadManager.remove(downloadID)
-                            downloadID = -10
-                            Log.i("CHARLESS", "in Main Activity Handler")
-                            flag = false
+                object : CountDownTimer(30000, 1000) {
+
+                    override fun onTick(millisUntilFinished: Long) {
+                        if (custom_button.buttonState == ButtonState.Completed) {
+                            cancel()
+                            Log.i("CHARLESS", "countdowntimer has been cancelled")
                         }
-                        else {flag = !flag}
-                    }, 30000)
-                }
+                    }
+
+                    override fun onFinish() {
+                        custom_button.buttonState = ButtonState.Indeterminate
+                        //https://stackoverflow.com/questions/39271027/cancel-download-in-downloadmanager
+                        // stop the download, its taking too long for such a small file
+                        downloadManager.remove(downloadID)
+                        downloadID = -10
+                        Log.i("CHARLESS", "in Main Activity Handler")
+
+                    }
+                }.start()
             }
-            // This was helpful: https://knowledge.udacity.com/questions/420421
-           // clickMade()
         }
+        // This was helpful: https://knowledge.udacity.com/questions/420421
+        // clickMade()
+    }
 
 
     private val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context?, intent: Intent?) {
             id = intent?.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1)
-            if ( id == downloadID) {
+            if (id == downloadID) {
                 Log.i("CHARLESS:", "download completed")
                 custom_button.buttonState = ButtonState.Completed
+                val notificationManager = ContextCompat.getSystemService(
+                    context!!,
+                    NotificationManager::class.java
+                ) as NotificationManager
+                notificationManager.sendNotification("testing", applicationContext)
+
             }
         }
     }
@@ -129,15 +150,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-      //  https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip
+        //  https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip
 
 
         private const val glideURL =
             "https://github.com/bumptech/glide/archive/master.zip"
-       // private const val udacityURL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+
+        // private const val udacityURL = "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
         private const val udacityURL = "https://www.fakeurlblahblah.com"
         private const val retrofitURL = "https://github.com/square/retrofit/archive/master.zip"
-        private const val CHANNEL_ID = "channelId"
+      //  private const val CHANNEL_ID = "channelId"
     }
 /*
     private fun scaleButton() {
@@ -148,10 +170,10 @@ class MainActivity : AppCompatActivity() {
 */
 
     fun clickMade() {
-       custom_button.animateView()
+        custom_button.animateView()
     }
 
-    fun findOutWhichRadioButtonIsSelected() : String {
+    fun findOutWhichRadioButtonIsSelected(): String {
         val radioSelected = radioGroup.checkedRadioButtonId
         Log.i("CHARLESradio selected:", R.id.radio_udacity.toString())
         return when (radioSelected) {
@@ -162,4 +184,68 @@ class MainActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                .apply {
+                    setShowBadge(true)
+                }
+            notificationChannel.description = "downloaded status"
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+    }
+
+    private val NOTIFICATION_ID = 0
+    private val REQUEST_CODE = 0
+    private val FLAGS = 0
+
+    fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val detailIntent = Intent(applicationContext, DetailActivity::class.java)
+        val detailPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            REQUEST_CODE,
+            detailIntent,
+            FLAGS)
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.download_notification_channel_id)
+        )
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle(applicationContext
+                .getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+          //  .setChannelId(getString(R.string.download_notification_channel_id))
+                /*
+            .addAction(
+                R.drawable.ic_launcher_foreground,
+                applicationContext.getString(R.string.notification_button),
+                detailPendingIntent
+            )
+                 */
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+             notify(NOTIFICATION_ID, builder.build())
+        Log.i("CHARLESX", "completed sendNotification")
+    }
 }
+
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
+
---
+++ app/src/main/java/com/udacity/Utils/NotificationUtils.kt
@@ -0,0 +1,56 @@
+package com.udacity.Utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import androidx.core.app.NotificationCompat
+import com.udacity.DetailActivity
+import com.udacity.MainActivity
+import com.udacity.R
+
+class NotificationUtils {
+    // Notification ID.
+    private val NOTIFICATION_ID = 0
+    private val REQUEST_CODE = 0
+    private val FLAGS = 0
+
+    fun NotificationManager.sendNotification(messageBody: String, applicationContext: Context) {
+        val contentIntent = Intent(applicationContext, MainActivity::class.java)
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_ONE_SHOT
+        )
+
+        val detailIntent = Intent(applicationContext, DetailActivity::class.java)
+        val detailPendingIntent: PendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            REQUEST_CODE,
+            detailIntent,
+            FLAGS)
+
+        val builder = NotificationCompat.Builder(
+            applicationContext,
+            applicationContext.getString(R.string.download_notification_channel_id)
+        )
+            .setContentTitle(applicationContext
+                .getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true)
+            .addAction(
+                R.drawable.ic_launcher_foreground,
+                applicationContext.getString(R.string.notification_button),
+                detailPendingIntent
+            )
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+        notify(NOTIFICATION_ID, builder.build())
+    }
+}
+
+fun NotificationManager.cancelNotifications() {
+    cancelAll()
+}
